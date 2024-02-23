c71d11397523070135a8082e5f8beb75176f7735
Clyde Tedrick <clyde.tedrick@tealium.com>
2024-02-23 12:50:53 -0500

    no mocking

--- tests/test_cookiecutterplus.py
+++ tests/test_cookiecutterplus.py
@@ -1,22 +1,8 @@
-import pytest
-from unittest.mock import patch, MagicMock
+from pathlib import Path
 from cookiecutterplus import CookieCutterPlusSkinny
 
-@pytest.fixture
-def mock_determine_repo_dir():
-    with patch('cookiecutter.repository.determine_repo_dir') as mock:
-        yield mock
 
-@pytest.fixture
-def mock_cookiecutter():
-    with patch('cookiecutter.main.cookiecutter') as mock:
-        yield mock
-
-def test_cookiecutter_plus_skinny_runs(mock_determine_repo_dir, mock_cookiecutter):
-    # Setup mock return values
-    mock_determine_repo_dir.return_value = ("mock_template_path", "mock_repo_dir")
-    mock_cookiecutter.return_value = None  # Assuming cookiecutter returns None for success
-
+def test_cookiecutter_plus_skinny_runs(tmp_path):
     # Define your test payload and output path
     test_payload = {
         "template1": {
@@ -24,25 +10,8 @@ def test_cookiecutter_plus_skinny_runs(mock_determine_repo_dir, mock_cookiecutte
             "context_vars": {"key1": "value1", "key2": "value2"}
         }
     }
-    test_output_path = "test/output/path"
+    test_output_path = Path(tmp_path, 'test/output/path')
 
     # Instantiate and run your class
     ccp = CookieCutterPlusSkinny(payload=test_payload, output_path=test_output_path)
     ccp.run()
-
-    # Assertions
-    mock_determine_repo_dir.assert_called_once_with(
-        template=ccp.github_repo_prefix,
-        directory="cookiecutter/shared/cookiecutter-gha",
-        checkout="main",
-        clone_to_dir="./template_repo",
-        no_input=True,
-        abbreviations="gh"
-    )
-    mock_cookiecutter.assert_called_once_with(
-        template="mock_template_path",
-        no_input=True,
-        overwrite_if_exists=True,
-        extra_context={"key1": "value1", "key2": "value2"},
-        output_dir=test_output_path
-    )
