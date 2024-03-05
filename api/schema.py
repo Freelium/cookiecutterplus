8e1d5fd8b7dedd1e701d7cf53d764820424f9424
Ernest Ramirez <ernest.ramirez@tealium.com>
2024-03-05 13:10:04 -0600

    we are gonna have to make this a bit fancier

--- api/schema.py
+++ api/schema.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify
 from marshmallow import Schema, fields, ValidationError
 
-class TemplatePayloadSchema(Schema):
-    template_context = fields.String(required=True)
-    template_path = fields.String(required=True)
-    context_vars = fields.Dict(required=True)
-
-class PersistenceSchema(Schema):
-    destination = fields.String(required=True)
-    persistence_type = fields.String(required=True)
 
 class MainSchema(Schema):
     no_input = fields.Boolean(required=False)
     output_path = fields.String(required=True)
     persistence = fields.Nested(PersistenceSchema, required=True)
     template_payload = fields.Nested(TemplatePayloadSchema, required=True)
+
+class TemplatePayloadSchema(Schema):
+    payload_dict = fields.Dict(required=True)
+
+class CookieCutterTemplateSchema(Schema):
+    template_context = fields.String(required=True)
+    template_path = fields.String(required=True)
+    context_vars = fields.Dict(required=True)
+
+class PersistenceSchema(Schema):
+    github = fields.Nested(RepoSchema, required=False, missing=None, data_key="github")
+    gh = fields.Nested(RepoSchema, required=False, missing=None)
+
+class RepoSchema(Schema):
+    destination = fields.String(required=True)
+    repo_type = fields.String(required=True, validate=lambda x: x in ['public', 'private'])
