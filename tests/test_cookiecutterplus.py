4cff2a6987c062d78f09cea7cd185c1d5e9cf920
Ernest Ramirez <ernest.ramirez@tealium.com>
2024-03-06 14:40:36 -0600

    adding api tests

--- tests/test_cookiecutterplus.py
+++ tests/test_cookiecutterplus.py
@@ -1,8 +1,10 @@
-import yaml
-import pytest
+import yaml, pytest, requests
 from jsonschema import ValidationError
-from cookiecutterplus import CookieCutterPlus
+from cookiecutterplus import CookieCutterPlus, CookieCutterPlusAPI
 
+@pytest.fixture
+def api_url():
+    return 'http://localhost:5000/generate'
 
 # def test_cookiecutter_basic_persistence_github(tmp_path):
 #     test_payload = {
@@ -39,6 +41,33 @@ from cookiecutterplus import CookieCutterPlus
 #     with open(simple_yaml_file) as f:
 #         data = yaml.safe_load(f)
 #         assert data == {"name": "my-cut-cookie"}
+
+def test_cookiecutter_api(tmp_path):
+    api = CookieCutterPlusAPI()
+    cookiecutterplus_payload = {
+         "template_payload": {
+            "gha": {
+                "template_context": "tests/fixtures/basic-backwards",
+                "template_path": "",
+                "context_vars": {
+                    "component_name": "My Cut Cookie",
+                    "project_name": "my-cut-cookie",
+                    "project_slug": "my-cut-cookie"
+                }
+            },
+        },
+        "output_path": tmp_path,
+        "no_input": True1  
+    }
+    # Send a POST request to the API
+    response = requests.post(api_url, json=cookiecutterplus_payload)
+    # Assert the response status code is 201
+    assert response.status_code == 201
+    # Assert the response message is 'CookieCutter generation completed successfully'
+    assert response.json()['message'] == 'CookieCutter generation completed successfully'
+    
+
+
 
 def test_cookiecutter_basic_backwards_compat(tmp_path):
     test_payload = {
