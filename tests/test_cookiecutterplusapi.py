495ac4fb264ba2739f70dd48e5d42d391072a127
Ernest Ramirez <ernest.ramirez@tealium.com>
2024-03-08 12:26:42 -0600

    adding api persistence test

--- tests/test_cookiecutterplusapi.py
+++ tests/test_cookiecutterplusapi.py
@@ -2,7 +2,7 @@ import json, yaml, pytest, requests
 from unittest.mock import patch, MagicMock
 from api import CookieCutterPlusAPI
 
-
+# Create a fixture to instantiate a Flask test client with the CookieCutterPlusAPI
 @pytest.fixture
 def client():
    api = CookieCutterPlusAPI()
@@ -11,7 +11,7 @@ def client():
    with app.test_client() as client:
        yield client 
 
-
+# Test the CookieCutterPlusAPI with a valid payload
 def test_cookiecutter_api_success(tmp_path, client):
     cookiecutterplus_payload = {
         "template_payload": {
@@ -33,7 +33,7 @@ def test_cookiecutter_api_success(tmp_path, client):
     # Assert the response status code is 201
     assert response.status_code == 201
 
-
+# Test the CookieCutterPlusAPI with an invalid parameter included in the payload
 def test_cookiecutter_api_invalid_parameter(tmp_path, client):
     api = CookieCutterPlusAPI()
     cookiecutterplus_payload = {
@@ -58,3 +58,35 @@ def test_cookiecutter_api_invalid_parameter(tmp_path, client):
     response = client.post('/generate', json=cookiecutterplus_payload)
     # Assert the response status code is 201
     assert response.status_code == 400
+
+# Import the githubpersistence class
+@patch('persistence.githubpersistence.GithubPersistence.persist')
+# Test the CookieCutterPlusAPI with a persistence payload included
+def test_cookiecutter_api_persistence(tmp_path, client, mock_github_persistence):
+    cookiecutterplus_payload = {
+        "template_payload": {
+            "gha": {
+                "template_context": "tests/fixtures/basic-backwards",
+                "template_path": "",
+                "context_vars": {
+                    "component_name": "My Cut Cookie",
+                    "project_name": "my-cut-cookie",
+                    "project_slug": "my-cut-cookie"
+                }
+            },
+        },
+        "output_path": f"{tmp_path}",
+        "no_input": True,
+        "persistence": {
+            "gh": {
+                "destination": "test/repo",
+                "repo_type": "private"
+            }
+        }
+    }
+    # Send a POST request to the API
+    response = client.post('/generate', json=cookiecutterplus_payload)
+    # Assert the response status code is 201
+    assert response.status_code == 201
+    instance = mock_github_persistence.return_value
+    instance.persist.assert_called_once_with("test/repo", "private")
