f6ed0e5ce8a5a51b813df29cf87536b156f51b84
Ernest Ramirez <ernest.ramirez@tealium.com>
2024-03-08 10:37:00 -0600

    missing ()

--- tests/test_cookiecutterplusapi.py
+++ tests/test_cookiecutterplusapi.py
@@ -26,7 +26,7 @@ def test_cookiecutter_api(tmp_path):
         "no_input": True
     }
     # Send a POST request to the API
-    response = requests.post(api_url, json=cookiecutterplus_payload)
+    response = requests.post(api_url(), json=cookiecutterplus_payload)
     # Assert the response status code is 201
     assert response.status_code == 201
     # Assert the response message is 'CookieCutter generation completed successfully'
