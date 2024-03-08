0437f1601bdf53f6fa23ad69c635fad9cfef44b5
Ernest Ramirez <ernest.ramirez@tealium.com>
2024-03-08 13:57:44 -0600

    adding comments

--- tests/test_cookiecutterpluspersistence.py
+++ tests/test_cookiecutterpluspersistence.py
@@ -1,7 +1,7 @@
 from persistence import PersistenceBuilder, GithubPersistence
 import pytest
 
-
+# Test the PersistenceBuilder factory returns the GitHub persistence class
 def test_persistence_factory_github():
     persistence = PersistenceBuilder.get_persister('gh')
     assert isinstance(persistence, GithubPersistence)
