8ec3a2f1bc347d4846420b0898eb65c0a71f1700
Ernest Ramirez <ernest.ramirez@tealium.com>
2024-03-11 12:06:12 -0500

    adding cli persistence test

--- tests/test_cookiecutterplus.py
+++ tests/test_cookiecutterplus.py
@@ -1,6 +1,53 @@
 import yaml, pytest, requests
 from jsonschema import ValidationError
 from cookiecutterplus import CookieCutterPlus
+from unittest.mock import patch, MagicMock
+
+
+# Import the persistencebuilder from cookiecutterplus class
+@patch('cookiecutterplus.cookiecutterplus.PersistenceBuilder.get_persister')
+# Test the CookieCutterPlusAPI with a persistence payload included
+def test_cookiecutter_persistence(mock_get_persister, tmp_path):
+    cookiecutterplus_payload = {
+        "template_payload": {
+            "gha": {
+                "template_context": "tests/fixtures/basic-backwards",
+                "template_path": "",
+                "context_vars": {
+                    "component_name": "My Cut Cookie",
+                    "project_name": "my-cut-cookie",
+                    "project_slug": "my-cut-cookie"
+                }
+            },
+        },
+        "output_path": f"{tmp_path}",
+        "no_input": True,
+        "persistence": {
+            "gh": {
+                "destination": "test/repo",
+                "repo_type": "private"
+            }
+        }
+    }
+    mock_persistence_instance = MagicMock()
+    mock_get_persister.return_value = mock_persistence_instance
+    # Instantiate and run your class
+    ccp = CookieCutterPlus(test_payload)
+    ccp.run()
+    # Assert template was created
+    dir = tmp_path / "my-cut-cookie"
+    simple_yaml_file = dir / "simple.yaml"
+
+    assert dir.is_dir()
+    assert simple_yaml_file.is_file()
+    with open(simple_yaml_file) as f:
+        data = yaml.safe_load(f)
+        assert data == {
+            "name": "my-cut-cookie",
+            "additive": "this is additive"
+        }
+    # Assert the persistence was called
+    mock_persistence_instance.persist.assert_called_once()
 
 
 def test_cookiecutter_basic_backwards_compat(tmp_path):
