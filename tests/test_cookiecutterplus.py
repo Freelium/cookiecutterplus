f6d8a89e7065ccde6edc946e811adf0c1d0919c2
Ernest Ramirez <ernest.ramirez@tealium.com>
2024-03-11 10:12:41 -0500

    woot, tests work

--- tests/test_cookiecutterplus.py
+++ tests/test_cookiecutterplus.py
@@ -3,46 +3,6 @@ from jsonschema import ValidationError
 from cookiecutterplus import CookieCutterPlus
 
 
-# def test_cookiecutter_basic_persistence_github(tmp_path):
-#     test_payload = {
-#         "output_path": tmp_path,
-#         "payload": {
-#             "gha": {
-#                 "template_context": "tests/fixtures/basic-backwards",
-#                 "template_path": "",
-#                 "context_vars": {
-#                     "component_name": "My Cut Cookie",
-#                     "project_name": "my-cut-cookie",
-#                     "project_slug": "my-cut-cookie"
-#                 }
-#             }
-#         },
-#         "persistence": {
-#             "github": {
-#                 "destination": "my-org/my-repo",
-#                 "repo_type": "public"
-#             }
-#         },
-#         "no_input": True
-#     }
-
-#     # Instantiate and run your class
-#     ccp = CookieCutterPlus(test_payload)
-#     ccp.run()
-
-#     dir = tmp_path / "my-cut-cookie"
-#     simple_yaml_file = dir / "simple.yaml"
-
-#     assert dir.is_dir()
-#     assert simple_yaml_file.is_file()
-#     with open(simple_yaml_file) as f:
-#         data = yaml.safe_load(f)
-#         assert data == {"name": "my-cut-cookie"}
-
-
-
-
-
 def test_cookiecutter_basic_backwards_compat(tmp_path):
     test_payload = {
         "output_path": tmp_path,
