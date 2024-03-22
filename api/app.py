79d69475dd4f6916a807fc326829103ec9a328c7
Clyde Tedrick <clyde.tedrick@tealium.com>
2024-03-22 14:32:26 -0400

    cors

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from marshmallow import ValidationError
 from waitress import serve
 from cookiecutterplus import CookieCutterPlus 
@@ -8,23 +9,27 @@ from .schema import MainSchema
 class CookieCutterPlusAPI:
     def __init__(self):
         self.app = Flask(__name__)
+        CORS(self.app, resources={r"/*": {"origins": "*"}})
         self.app.route('/generate', methods=['POST'])(self.generate)
 
     @staticmethod
     def generate():
         # Validate the incoming request's content using MainSchema
         schema = MainSchema()
+        response = None
         try:
             print(f"Request JSON: {request.json}")
             valid_data = schema.load(request.json)
         except ValidationError as e:
-            return jsonify({'error': f"Invalid request {e}"}), 400
+            response = jsonify({'error': f"Invalid request {e}"}), 400
         # After Validation, Instantiate your CookieCutterPlus class and run the process
         try:
             CookieCutterPlus(valid_data).run()
-            return jsonify({'message': 'CookieCutter generation completed successfully'}), 201
+            response = jsonify({'message': 'CookieCutter generation completed successfully'}), 201
         except ValueError or ValidationError as e:
-            return jsonify({'error': f"Missing required parameters {e}"}), 400
+            response = jsonify({'error': f"Missing required parameters {e}"}), 400
+        
+        return response
 
     def get_flask_app(self):
         return self.app
